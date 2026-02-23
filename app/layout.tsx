4eef5f98e0c64f3858c2b25fc4879c2eff620d4d
Dana Sharon <whosedreamisthis@gmail.com>
2026-02-23 13:48:21 -0800

    chef icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,6 +18,10 @@ const roboto = Roboto({
 export const metadata: Metadata = {
 	title: 'Recipe Manager',
 	description: 'recipe manager',
+	icons: {
+		icon:
+			'data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>👨‍🍳</text></svg>',
+	},
 };
 
 export default function RootLayout({
